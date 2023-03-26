5cb98f1c16e48c5739a7022f105b49d66d4e167f
KondratenkoAlexey <alexeykondratenko8@gmail.com>
2023-03-26 20:32:34 +0300

    create manual serializer

--- mysite/books/urls.py
+++ mysite/books/urls.py
@@ -4,5 +4,7 @@ from .views import index, BooksAPIView, AuthorsAPIView
 urlpatterns = [
     path('', index),
     path('api/v1/bookslist/', BooksAPIView.as_view()),
+    path('api/v1/bookslist/<int:pk>/', BooksAPIView.as_view()),
     path('api/v1/authorslist/', AuthorsAPIView.as_view()),
+    path('api/v1/authorslist/<int:pk>/', AuthorsAPIView.as_view()),
 ]
