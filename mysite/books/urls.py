b2faff8a82540c8a980571cb1b8516b03760728b
KondratenkoAlexey <alexeykondratenko8@gmail.com>
2023-03-26 21:24:58 +0300

    new API

--- mysite/books/urls.py
+++ mysite/books/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import index, BooksAPIView, AuthorsAPIView
+from .views import *
 
 urlpatterns = [
     path('', index),
     path('api/v1/bookslist/', BooksAPIView.as_view()),
-    path('api/v1/bookslist/<int:pk>/', BooksAPIView.as_view()),
+    path('api/v1/bookslist/<int:pk>/', BooksAPIDetailView.as_view()),
     path('api/v1/authorslist/', AuthorsAPIView.as_view()),
-    path('api/v1/authorslist/<int:pk>/', AuthorsAPIView.as_view()),
+    path('api/v1/authorslist/<int:pk>/', AuthorsAPIDetailView.as_view()),
 ]
